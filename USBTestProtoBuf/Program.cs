bac8ecb26b3c9b7921973b068c79eafffe1aa8db
semenshikov <semenshikov@ukr.net>
2019-09-22 16:14:06 +0300

    Добавьте файлы проекта.

---
+++ USBTestProtoBuf/Program.cs
@@ -0,0 +1,31 @@
+﻿using ProtoBuf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace USBTestProtoBuf
+{
+    class Program
+    {
+
+        [ProtoContract]
+        class SimpleMessage
+        {
+            [ProtoMember(1)]
+            public int lucky_number { get; set; }
+        }
+
+
+        static void Main(string[] args)
+        {
+            SimpleMessage simpleMessage = new SimpleMessage();
+            simpleMessage.lucky_number = 1;
+            var fs = File.Create("data.dat");
+            Serializer.Serialize(fs,simpleMessage);
+
+        }
+    }
+}
